54135d9409bd064dc4fa5cbc763d0142e1029464
Gordon Belbin <gordon.belbin@btinternet.com>
2021-01-19 07:44:49 +0000

    Added 2021 to the tracking

--- weeklydeaths.R
+++ weeklydeaths.R
@@ -8,12 +8,13 @@ newdata <- gather(deaths, 'WeekNo', 'Deaths', -Year)
 
 newdata$WeekNo <- as.integer(newdata$WeekNo)
 
-ggplot(newdata) + geom_boxplot(data=newdata[newdata$Year != 2020,], mapping=aes(x=WeekNo, y=Deaths, group=WeekNo)) +
+ggplot(newdata) + geom_boxplot(data=newdata[newdata$Year < 2020,], mapping=aes(x=WeekNo, y=Deaths, group=WeekNo)) +
   geom_point(data=newdata[newdata$Year == 2020 & newdata$Deaths != 0,], mapping=aes(x=WeekNo, y=Deaths), colour='red') +
+  geom_point(data=newdata[newdata$Year == 2021 & newdata$Deaths != 0,], mapping=aes(x=WeekNo, y=Deaths), colour='green') +
   scale_x_continuous(breaks=1:52, minor_breaks=NULL) +
   scale_y_continuous(limits=c(0, 25000)) +
   labs(title='Total Deaths in England and Wales per Week from All Causes (ONS Data)',
-       subtitle='Boxplots (including black dots) indicate the spread of weekly deaths for 2010-2019\nRed dots indicate the total weekly deaths so far in 2020',
+       subtitle='Boxplots (including black dots) indicate the spread of weekly deaths for 2010-2019\nRed dots indicate the total weekly deaths so far in 2020 and 2021',
        x='Week Number', y='Total Deaths from All Causes in England and Wales')
 
 ggsave("weeklydeaths.png")
